b9869836bedce3c872afedb6a1461b83210106c1
CarlosLuz <carlosluz12892@gmail.com>
2024-04-24 09:32:29 -0300

    teste URL api prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: '/https://api-rest-pi-nine.vercel.app',
+  baseUrl: 'https://api-rest-pi-nine.vercel.app',
   mapboxKey: 'pk.eyJ1IjoiY2FybG9zN3V6IiwiYSI6ImNsdXduODFsdDBmYWkya21razFrNnFhbHYifQ.K_7zB_-b-u0ryrsXyZPm9w'
 };
 
